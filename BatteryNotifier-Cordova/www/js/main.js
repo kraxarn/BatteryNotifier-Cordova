ea9bd9b9e1495c6e3ca3360f5d09a0a4f8927099
kraxarn <emil202821@hotmail.com>
2018-08-28 18:27:06 +0200

    Initial app

--- BatteryNotifier-Cordova/www/js/main.js
+++ BatteryNotifier-Cordova/www/js/main.js
@@ -10,6 +10,9 @@ function onDeviceReady()
     // Handle pause and resume events
     document.addEventListener("pause", onPause, false)
     document.addEventListener("resume", onResume, false)
+
+    // Handle battery events
+    window.addEventListener("batterystatus", onBatteryStatus, false)
 }
 
 function onPause()
@@ -22,5 +25,25 @@ function onResume()
     // TODO
 }
 
+function onBatteryStatus(status)
+{
+    /*
+     * Battery level: status.level
+     * Charging:      status.isPlugged
+     */
+    
+    window.currentBattery.src = `img/${Math.round(status.level / 10) * 10}.svg`
+    window.currentBatteryLevel.textContent = `${status.level}%`
+}
+
 // Vars
 const storage = window.localStorage
+
+// Update values when changing slider
+highBatteryLevel.addEventListener("input", () => {
+    window.highBatteryLabel.textContent = `${window.highBatteryLevel.value}%`
+})
+
+lowBatteryLevel.addEventListener("input", () => {
+    window.lowBatteryLabel.textContent = `${window.lowBatteryLevel.value}%`
+})
